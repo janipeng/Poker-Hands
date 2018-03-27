4654384c9c02f2cd7022d6bc7174e1c1b29af2f9
Jani Peng <jani.peng@cargosmart.com>
2018-03-27 20:49:22 +0800

    some refactoring

--- src/main/java/org/jani/Card.java
+++ src/main/java/org/jani/Card.java
@@ -1,6 +1,8 @@
 package org.jani;
 
-class Card {
+import static java.lang.Integer.parseInt;
+
+class Card implements Comparable<Card> {
   private String value;
   private String suit;
 
@@ -8,5 +10,14 @@ class Card {
     String[] cardInfos = card.split("");
     this.value = cardInfos[0];
     this.suit = cardInfos[1];
+  }
+
+  int getValue() {
+    return parseInt(value);
+  }
+
+  @Override
+  public int compareTo(Card o) {
+    return this.getValue() - o.getValue();
   }
 }
--- src/main/java/org/jani/PokerHands.java
+++ src/main/java/org/jani/PokerHands.java
@@ -2,18 +2,29 @@ package org.jani;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
-class PokerHands {
+import static java.util.Arrays.stream;
+import static java.util.stream.Collectors.toList;
 
-  private final List<Card> cards = new ArrayList<>();
+class PokerHands implements Comparable<PokerHands>  {
 
-  PokerHands(String input) {
-    for (String card : input.split(" ")) {
-      this.cards.add(new Card(card));
-    }
+  private List<Card> cards = new ArrayList<>();
+  private String name;
+
+  PokerHands(String name, String input) {
+    this.name = name;
+    cards = stream(input.split(" "))
+            .map(Card::new)
+            .sorted()
+            .collect(toList());
   }
 
-  String vs(PokerHands pokerHands) {
-    return "";
+  @Override
+  public int compareTo(PokerHands o) {
+    return IntStream.range(0, 5)
+            .map(index -> cards.get(index).compareTo(o.cards.get(index)))
+            .filter(integer -> integer != 0)
+            .findFirst().orElse(0);
   }
 }
