3b28daaad982e8d32e5bf5e8ee680944d1ecbee9
Jani Peng <1184360257@qq.com>
2018-04-05 17:11:20 +0800

    add draw case and refactoring

--- src/main/java/org/jani/PokerHands.java
+++ src/main/java/org/jani/PokerHands.java
@@ -2,12 +2,14 @@ package org.jani;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.IntStream;
 
 import static java.util.Arrays.stream;
+import static java.util.Optional.ofNullable;
 import static java.util.stream.Collectors.toList;
 
-class PokerHands implements Comparable<PokerHands>  {
+class PokerHands implements Comparable<PokerHands> {
 
   private List<Card> cards = new ArrayList<>();
   private String name;
@@ -15,27 +17,33 @@ class PokerHands implements Comparable<PokerHands>  {
   PokerHands(String name, String input) {
     this.name = name;
     cards = stream(input.split(" "))
-            .map(Card::new)
-            .sorted()
-            .collect(toList());
+        .map(Card::new)
+        .sorted()
+        .collect(toList());
   }
 
   @Override
   public int compareTo(PokerHands o) {
     return IntStream.range(0, 5)
-            .map(index -> cards.get(index).compareTo(o.cards.get(index)))
-            .filter(integer -> integer != 0)
-            .findFirst().orElse(0);
+        .map(index -> cards.get(index).compareTo(o.cards.get(index)))
+        .filter(integer -> integer != 0)
+        .findFirst().orElse(0);
   }
 
-  public String vs(PokerHands pokerHands2) {
-    int compareTo = this.compareTo(pokerHands2);
-    if (compareTo > 0) {
-      return this.name + " wins.";
+  public String battle(PokerHands poker) {
+    return ofNullable(winner(poker))
+        .orElse("Nobody")
+        .concat(" wins.");
+  }
+
+  private String winner(PokerHands poker) {
+    int compareResult = this.compareTo(poker);
+    if (compareResult > 0) {
+      return this.name;
     }
-    if (compareTo < 0) {
-      return pokerHands2.name + " wins.";
+    if (compareResult < 0) {
+      return poker.name;
     }
-    return "";
+    return null;
   }
 }
--- src/test/java/org/jani/PokerHandsTest.java
+++ src/test/java/org/jani/PokerHandsTest.java
@@ -12,11 +12,20 @@ public class PokerHandsTest {
     PokerHands pokerHands1 = new PokerHands("Player 1", "2H 3D 5S 4C 6D");
     PokerHands pokerHands2 = new PokerHands("Player 2", "2C 3H 4S 8C 5H");
 
-    String result = pokerHands1.vs(pokerHands2);
+    String result = pokerHands1.battle(pokerHands2);
 
     assertThat(result, is("Player 2 wins."));
   }
 
+  @Test
+  public void highCardAndAllValuesLessThan10_sameValues() throws Exception {
+    PokerHands pokerHands1 = new PokerHands("Player 1", "2H 3D 5S 4C 6D");
+    PokerHands pokerHands2 = new PokerHands("Player 2", "2C 3H 5S 4C 6H");
+
+    String result = pokerHands1.battle(pokerHands2);
+
+    assertThat(result, is("Nobody wins."));
+  }
 
 }
 
